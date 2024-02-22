64135661d3524fc76cf920d4562d79a293c22625
minaaadev <minaaadev@gmail.com>
2024-02-22 12:39:32 +0900

    [Bronze IV] Title: No Brainer, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.py
@@ -0,0 +1,10 @@
+n=int(input())
+while n:
+    x,y=map(int,input().split())
+    if x==y:
+        print("MMM BRAINS")
+    elif x>y:
+        print("MMM BRAINS")
+    elif x<y:
+        print("NO BRAINS")
+    n-=1
