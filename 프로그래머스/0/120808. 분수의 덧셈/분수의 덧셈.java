85521a27939878749d522d2a8a447d9e62e6fac7
minaaadev <minaaadev@gmail.com>
2025-02-11 13:52:14 +0900

    [level 0] Title: 분수의 덧셈, Time: 5.33 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        
+        int numer = numer1*denom2 + numer2*denom1; 
+        int denom = denom1*denom2;
+        
+        int max = 1;
+        
+        for (int i =1; i<=numer && i<=denom; i++) {
+            if (denom%i == 0 && numer%i == 0 ) {
+             max = i; 
+            }
+        }
+        
+        //최소공배수로 각각 나누기
+        numer=numer/max;
+        denom=denom/max;
+        
+        int[] answer={numer, denom};
+        
+        
+        return answer;
+    }
+}
